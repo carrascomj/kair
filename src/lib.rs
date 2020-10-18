068135361102c0633723037ecb73419251203f8f
carrascomj <carrascomurielj@gmail.com>
2020-10-18 21:31:06 +0200

    docs: improve API docs :bulb:

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,37 @@
-//! Constraint-Based Reconstruction and Analysis in Rust.
+//! Constraint-Based Reconstruction and Analysis.
 //! It uses the [rust_sbml](https://docs.rs/rust_sbml/0.3.0/rust_sbml/) to read a
 //! [SBML](http://sbml.org/Special/specifications/sbml-level-3/version-2/core/release-2/sbml-level-3-version-2-release-2-core.pdf)
-//! document and translates it into a LP formulation of Flux Balance Analysis using [lp_modeler](https://jcavat.github.io/rust-lp-modeler/lp_modeler/index.html)
+//! document and translates it into a LP formulation using [lp_modeler](https://jcavat.github.io/rust-lp-modeler/lp_modeler/index.html)
+//!
+//! # COBRA methods available
+//!
+//! * [Flux Balance Analysis](https://www.ncbi.nlm.nih.gov/pmc/articles/PMC3108565/).
+//!
+//! # Examples
+//!
+//! Read and optimize [e_coli_core model](http://bigg.ucsd.edu/models/e_coli_core)
+//! ```
+//! use kair::ModelLP;
+//! use std::str::FromStr;
+//!
+//! let file_str = std::fs::read_to_string("tests/EcoliCore.xml").unwrap();
+//! let model = ModelLP::from_str(&file_str).unwrap();
+//! println!(
+//!     "Model has {:?} constraints and {:?} variables",
+//!     &model.constraints.len(),
+//!     &model.variables.len()
+//! );
+//! for (name, val) in model.optimize().unwrap().iter() {
+//!     println!("{} = {}", name, val)
+//! }
+//! ```
+//!
+//! # Additional links
+//!
+//! * [Github repository](https://github.com/carrascomj/kair), open to PRs!
+//! * [rust_sbml](https://docs.rs/rust_sbml/0.3.0/rust_sbml/): SBML parser in rust.
+//! * [cobrapy](https://github.com/opencobra/cobrapy/): fully featured COBRA package written in Python.
+
 use lp_modeler::dsl::*;
 use lp_modeler::solvers::{CbcSolver, SolverTrait};
 use rust_sbml::{Model, Parameter, Reaction, Specie, SpeciesReference};
@@ -10,33 +40,42 @@ use std::collections::HashMap;
 use std::ops::AddAssign;
 use std::str::FromStr;
 
-/// LP problem as an FBA formulation.
+/// LP problem as a Flux Balance Analysis formulation.
 ///
 /// See: [What is flux balance analysis?, Orth et al., 2010](https://www.ncbi.nlm.nih.gov/pmc/articles/PMC3108565/)
 ///
-/// Being $f(\overrightarrow{z})$ a function to optimize (historically, biomas or ATPase), $S$ and
+/// Being $f(\overrightarrow{z})$ a function to optimize (historically, biomass or ATPase), $S$ and
 /// stoichimetry matrix and $\overrightarrow{v}$ the flux vector representing the reactions in the reconstruction:
 ///
 /// $$ \text{Max.} f(\overrightarrow{z}) \newline \text{subject to}\medspace S\overrightarrow{v} = 0 \newline \text{where}\medspace lb_j \le v_j \le ub_j $$
 pub struct ModelLP {
+    /// Metabolites from the SBML document
     pub metabolites: HashMap<String, Specie>,
+    /// Reactions from the SBML document
     pub reactions: HashMap<String, Reaction>,
+    /// Parsed from reactions, variables of LP problem
     pub variables: HashMap<String, LpExpression>,
+    /// Parameters from the SBML document
     pub config: HashMap<String, Parameter>,
+    /// Reaction id to be used as the objective in the LP problem
     pub objective: String,
     obj_expr: Option<LpExpression>,
     stoichiometry: HashMap<String, Vec<LpExpression>>,
+    /// Parsed from the stoichiometry matrix
     pub constraints: Vec<LpConstraint>,
 }
 
-/// Indicate that the struct can be translated to a flux balance constraint.
+/// Indicate that the struct can be translated to a flux balance variable (generally, reactions)
 pub trait Fbc {
+    /// Lower bound for the variable
     fn lb(&self, _parameters: &HashMap<String, Parameter>) -> f32 {
         0.
     }
+    /// Upper bound for the variable
     fn ub(&self, _parameters: &HashMap<String, Parameter>) -> f32 {
         0.
     }
+    /// Name of the variable to be used in the LP formulation
     fn name_var(&self) -> String {
         "".to_string()
     }
@@ -108,7 +147,7 @@ impl ModelLP {
         model.populate_model();
         model
     }
-    /// Optimizes the model according to Flux Balance Analysis (FBA).
+    /// Optimize the model according to Flux Balance Analysis (FBA).
     /// FBA: [https://pubmed.ncbi.nlm.nih.gov/20212490/](https://pubmed.ncbi.nlm.nih.gov/20212490/)
     ///
     /// # Example
