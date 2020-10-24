2f0877413c4724f4057f1b3d866ba7fa6b40e653
carrascomj <carrascomurielj@gmail.com>
2020-10-25 01:36:56 +0200

    style: clippy float comparison :art:

--- benches/formulate_from_sbml.rs
+++ benches/formulate_from_sbml.rs
@@ -17,11 +17,11 @@ fn optimize_ecoli() {
 }
 
 fn create_lp_benchmark(c: &mut Criterion) {
-    c.bench_function("Populate E. coli core", |b| b.iter(|| read_ecoli()));
+    c.bench_function("Populate E. coli core", |b| b.iter(read_ecoli));
 }
 
 fn optimize_benchmark(c: &mut Criterion) {
-    c.bench_function("Optimize E. coli core", |b| b.iter(|| optimize_ecoli()));
+    c.bench_function("Optimize E. coli core", |b| b.iter(optimize_ecoli));
 }
 
 criterion_group!(benches, optimize_benchmark, create_lp_benchmark);
--- tests/ecoli.rs
+++ tests/ecoli.rs
@@ -3,7 +3,7 @@ extern crate kair;
 use kair::{Fbc, ModelLP};
 use std::str::FromStr;
 
-const EXAMPLE: &'static str = include_str!("../tests/EcoliCore.xml");
+const EXAMPLE: &str = include_str!("../tests/EcoliCore.xml");
 
 #[test]
 fn read_ecoli() {
@@ -13,7 +13,7 @@ fn read_ecoli() {
 #[test]
 fn verify_bound() {
     let model = ModelLP::from_str(&EXAMPLE).unwrap();
-    assert_eq!(model.reactions["R_ATPM"].lb(&model.config), 8.39);
+    assert_eq!((model.reactions["R_ATPM"].lb(&model.config) * 100.).round() as i32, 839);
 }
 
 #[test]
@@ -28,14 +28,14 @@ fn verify_neg_bound() {
             .filter(|id| id.starts_with("R_EX"))
             .collect::<Vec::<String>>()
     );
-    assert_eq!(model.reactions["R_EX_glc__D_e"].lb(&model.config), -10.);
+    assert_eq!(model.reactions["R_EX_glc__D_e"].lb(&model.config).round() as i32, -10);
 }
 
 #[test]
 fn optimize_ecoli() {
     let model = ModelLP::from_str(&EXAMPLE).unwrap();
     assert_eq!(
-        (model.optimize().unwrap()["R_BIOMASS_Ecoli_core_w_GAM_"] * 10000.).round(),
-        8739.
+        (model.optimize().unwrap()["R_BIOMASS_Ecoli_core_w_GAM_"] * 10000.).round() as i32,
+        8739
     )
 }
